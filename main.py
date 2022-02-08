e1d0d318eae886d5c802334b70e5c24e18541a39
Anderson Sutil <andersonsutil.off@gmail.com>
2022-02-08 09:02:41 -0300

    variaveis em python

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+# utilizando esse repositorio para estudo da estrutura basica do python
+#
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+# metodo
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_soma(num1):
+    print({num1} + 54)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Utilizando variaveis
+    frase = 'A primeira luz que vi foi no dia:'
+    dia = 15
+    mes = 0o3
+    ano = 1998
+    print(frase, dia, mes, ano, sep="/", end="!\n")
